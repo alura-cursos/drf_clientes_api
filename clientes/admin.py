26c39bd77d999860e8f61cdc653a916819f3c6cb
Guilherme Lima <guilhermelimaruby@gmail.com>
2020-08-25 14:29:05 -0300

    aula_4

--- clientes/admin.py
+++ clientes/admin.py
@@ -4,9 +4,10 @@ from clientes.models import Cliente
 class Clientes(admin.ModelAdmin):
     list_display = ('id', 'nome', 'cpf', 'rg', 'celular', 'ativo')
     list_display_links = ('id', 'nome')
-    search_fields = ('nome',)
+    search_fields = ('nome','cpf',)
     list_filter = ('ativo',)
     list_editable = ('ativo',)
     list_per_page = 10
+    ordering = ('nome',)
 
 admin.site.register(Cliente, Clientes)
--- setup/settings.py
+++ setup/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'clientes',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -115,8 +116,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 25
+}
